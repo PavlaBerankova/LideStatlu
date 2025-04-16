e6cedfb67e1484c59f2e7e6f34bf299b5e471594
Pavla_Beránková <berankovap@hotmail.com>
2025-04-16 08:58:10 +0200

    Initial Commit

---
+++ LideStatlu/LideStatluApp.swift
@@ -0,0 +1,17 @@
+//
+//  LideStatluApp.swift
+//  LideStatlu
+//
+//  Created by Pavla Beránková on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LideStatluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
