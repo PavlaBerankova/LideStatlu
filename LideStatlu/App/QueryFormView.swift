9eb1be4b9c203ed39a5bfbb53793db3bd06cd653
Pavla_Beránková <berankovap@hotmail.com>
2025-04-16 10:35:57 +0200

    feat(ui): create main view, add assets, set accent color
    - main view with logo, app title and intro text
    - navigation link (button) to another screen

---
+++ LideStatlu/App/QueryFormView.swift
@@ -0,0 +1,20 @@
+//
+//  QueryFormView.swift
+//  LideStatlu
+//
+//  Created by Pavla Beránková on 16.04.2025.
+//
+
+import SwiftUI
+
+struct QueryFormView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Formulář pro uživatele")
+        }
+    }
+}
+
+#Preview {
+    QueryFormView()
+}
